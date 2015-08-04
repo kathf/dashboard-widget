71bab1c224f188cededa6831e85cc8edc5ff20df
Kath <you@example.com>
2015-08-04 16:47:45 +1000

    dashboard working

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,38 +1,47 @@
 (function(){
-  var app = angular.module('myApp',['templates','ngRoute','gridster']);
+  var app = angular.module('myApp',['templates','ngRoute']);
+
+  app.controller('DashboardCtrl', function(){
+    console.log("inside dashboard controller");
+    $(".gridster ul").gridster({
+        widget_margins: [10, 10],
+        widget_base_dimensions: [140, 140],
+    });
+  });
 
   app.config([ '$routeProvider', function($routeProvider) {
     $routeProvider
       .when("/",
         {
           templateUrl: "show.html",
-          // controller: "WidgetCtrl"
+          controller: "DashboardCtrl"
         }
       );
   }]);
 
-  app.directive('map', function(){
-    return {
-      restrict: 'A',
 
-      template: "<div id='map-canvas'></div>",
-
-      controller: function() {
-        var mapOptions;
-        var map;
-        function initialize() {
-          mapOptions = {
-            zoom: 8,
-            center: new google.maps.LatLng(-34.397, 150.644)
-          };
-          map = new google.maps.Map(document.getElementById('map-canvas'),
-            mapOptions);
-        };
-        initialize();
-      },
-      // controllerAs: 'WidgetCtrl'
-    }
-  });
+  // app.directive('map', function(){
+  //   return {
+  //     restrict: 'A',
+  //
+  //     template: "<div id='map-canvas'></div>",
+  //
+  //     controller: function() {
+  //       var mapOptions;
+  //       var map;
+  //       function initialize() {
+  //         mapOptions = {
+  //           zoom: 8,
+  //           center: new google.maps.LatLng(-34.397, 150.644)
+  //         };
+  //         map = new google.maps.Map(document.getElementById('map-canvas'),
+  //           mapOptions);
+  //       };
+  //       initialize();
+  //     },
+  //     // controllerAs: 'WidgetCtrl'
+  //   }
+  // });
 
 
 
